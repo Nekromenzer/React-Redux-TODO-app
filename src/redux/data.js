9554ca3fbb0ffbfd62ff0731b3cc3cc7d9e95ad0
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-03-20 20:14:34 +0530

    dummy data

---
+++ src/redux/data.js
@@ -0,0 +1,10 @@
+const todos = [
+  { id: 0.57, name: 'Learn Redux' },
+  { id: 0.58, name: 'Learn React' },
+  { id: 0.59, name: 'Learn React Native' },
+  { id: 0.6, name: 'Learn Node' },
+  { id: 0.61, name: 'Learn Express' },
+  { id: 0.62, name: 'Learn MongoDB' }
+]
+
+export default todos
