a355d811f604b174b6f24ad7a47a1ac2aeab9eea
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-03-20 21:03:55 +0530

    add todo saga

--- src/redux/app.sagas.js
+++ src/redux/app.sagas.js
@@ -1,14 +1,23 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects'
-import { deleteTodoSaga } from './app.actions'
+import { deleteTodoSaga, addTodoSaga } from './app.actions'
 
-export function * DeleteTodoSagaWatcher ({ payload: { id } }) {
+export function * addTodoSagaWatcher ({ payload }) {
+  yield put(addTodoSaga(payload))
+}
+
+export function * onAdd () {
+  yield takeLatest('ADD_TODO', addTodoSagaWatcher)
+}
+
+//delete
+export function * deleteTodoSagaWatcher ({ payload: { id } }) {
   yield put(deleteTodoSaga(id))
 }
 
 export function * onDelete () {
-  yield takeLatest('DELETE_TODO', DeleteTodoSagaWatcher)
+  yield takeLatest('DELETE_TODO', deleteTodoSagaWatcher)
 }
 
 export function * todos () {
-  yield all([onDelete()])
+  yield all([onDelete(), call(onAdd)])
 }
