7c88900115f973031f5a196e4a641c87cf18032b
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-03-20 20:51:26 +0530

    call list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
+import TodoInput from './components/TodoInput'
+import TodoItem from './components/TodoItem'
+import TodoList from './components/TodoList'
 
 function App () {
-  return <div className='App'>app</div>
+  return (
+    <div className='App'>
+      <TodoList />
+      <TodoInput />  
+    </div>
+  )
 }
 
 export default App
