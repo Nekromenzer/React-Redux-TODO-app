e02cae210dc2d01164c5237476961040475b0c54
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-03-20 20:58:32 +0530

    add input

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../redux/app.actions'
 
 const TodoInput = () => {
-  return <div>Todo input</div>
+  const [task, setTask] = useState('')
+  const dispatch = useDispatch()
+
+  const handleSubmit = () => {
+    dispatch(addTodo({ id: Math.random(), name: task }))
+    setTask('')
+  }
+  return (
+    <div>
+      <input type='text' value={task} onChange={e => setTask(e.target.value)} />
+      <button onClick={handleSubmit}>Add Task</button>
+    </div>
+  )
 }
 
 export default TodoInput
