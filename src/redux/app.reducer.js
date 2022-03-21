52c7028614a6211314cd36d2eb3ec2be75bcaef3
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-03-21 07:48:06 +0530

    reducer to update

--- src/redux/app.reducer.js
+++ src/redux/app.reducer.js
@@ -11,6 +11,25 @@ const appReducer = (state = todos, action) => {
       newTodos = [...state]
       newTodos = newTodos.filter(todo => todo.id !== action.payload)
       return newTodos
+    case 'UPDATE_TODO_SAGA':
+      newTodos = [...state]
+      let index = -1
+      //   for (let i = 0; i < newTodos.length; i++) {
+      //     index++
+      //     if (newTodos[i].id === action.payload.id) {
+      //       break
+      //     }
+      //     if (index !== -1) {
+      //       newTodos[index] = action.payload
+      //     }
+      //   }
+      for (let i = 0; i < newTodos.length; i++) {
+        if (newTodos[i].id === action.payload.id) {
+          newTodos[i] = action.payload
+          break
+        }
+      }
+      return newTodos
     default:
       return state
   }
