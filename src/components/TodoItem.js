3ea69bad16b27d53673c612ce755471a357b38ec
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2022-03-21 07:48:32 +0530

    to do iem functons

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { deleteTodo } from '../redux/app.actions'
+import { deleteTodo, updateTodo } from '../redux/app.actions'
 
 const TodoItem = ({ todo }) => {
+  const [edit, setEdit] = useState(false)
+  const [task, setTask] = useState(todo.name)
   const dispatch = useDispatch()
+
   return (
     <div>
-      {todo.name}
+      {edit ? (
+        <input
+          type='text'
+          onChange={e => setTask(e.target.value)}
+          value={task}
+        />
+      ) : (
+        <>{todo.name}</>
+      )}
+      <button
+        onClick={() => {
+          setEdit(!edit)
+          if (edit) {
+            setTask(todo.name)
+            dispatch(updateTodo({ ...todo, name: task }))
+          }
+        }}
+      >
+        {!edit ? 'Edit' : 'Update'}
+      </button>
       <button onClick={() => dispatch(deleteTodo({ id: todo.id }))}>
         Delete
       </button>
